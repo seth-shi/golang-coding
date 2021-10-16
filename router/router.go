ba18083e84fee331e3436b6f8b385ce5ae63eefa
施国鹏 <1033404553@qq.com>
2021-10-16 16:25:42 +0800

    add: ping route

--- router/router.go
+++ router/router.go
@@ -1,14 +1,15 @@
 package router
 
 import (
-	"golang-coding/app/api"
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
+	"golang-coding/app/api"
 )
 
 func init() {
 	s := g.Server()
 	s.Group("/", func(group *ghttp.RouterGroup) {
 		group.GET("/", api.Hello.Index)
+		group.GET("/ping", api.Hello.Ping)
 	})
 }
